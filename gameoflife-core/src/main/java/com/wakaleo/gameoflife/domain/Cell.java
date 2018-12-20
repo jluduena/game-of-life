9cde538d9a0a319343cfc93563c51e69632c3380
Juan Pablo Ludueña <jluduena@boxcustodia.com>
2018-12-20 15:40:52 -0300

    primer commit

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -1,43 +1,37 @@
 package com.wakaleo.gameoflife.domain;
 
- /**
- * Wakaleo Consulting - John Ferguson Smart
- * Game of life, demonstration application for Jenkins: The Definitive Guide
+/**
+ * Wakaleo Consulting - John Ferguson Smart Game of life, demonstration
+ * application for Jenkins: The Definitive Guide
  *
- * Cell.java
- * Cell object class, contains information on individual cells (positions) in the grid
+ * Cell.java Cell object class, contains information on individual cells
+ * (positions) in the grid
  */
 public enum Cell {
-	// Symbols to represent cell status
-    LIVE_CELL("*"), DEAD_CELL(".");
+	LIVE_CELL("+"), DEAD_CELL(".");
+	private String symbol;
 
-    private String symbol;
+	private Cell(String symbol) {
+		this.symbol = symbol;
+	}
 
-    private Cell(final String initialSymbol) {
-        this.symbol = initialSymbol;
-    }
+	@Override
+	public String toString() {
+		return symbol;
+	}
 
-	// Override function for easy printing of cell's symbol
-	// Functions identically to default getter ( getSymbol() )
-    @Override
-    public String toString() {
-        return symbol;
-    }
+	static Cell fromSymbol(String symbol) {
+		Cell cellRepresentedBySymbol = null;
+		for (Cell cell : Cell.values()) {
+			if (cell.symbol.equals(symbol)) {
+				cellRepresentedBySymbol = cell;
+				break;
+			}
+		}
+		return cellRepresentedBySymbol;
+	}
 
-	// Function used for creating a cell given a string
-    static Cell fromSymbol(final String symbol) {
-        Cell cellRepresentedBySymbol = null;
-        for (Cell cell : Cell.values()) {
-            if (cell.symbol.equals(symbol)) {
-                cellRepresentedBySymbol = cell;
-                break;
-            }
-        }
-        return cellRepresentedBySymbol;
-    }
-
-	// Default getter
-    public String getSymbol() {
-        return symbol;
-    }
+	public String getSymbol() {
+		return symbol;
+	}
 }
